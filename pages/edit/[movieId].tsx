86e6bf6aa818618eda4a3dfb763415a32df6ef1c
Miraaj Kadam <miraajkadam@gmail.com>
2022-02-12 19:22:43 +0530

    SSR for edit page

--- pages/edit/[movieId].tsx
+++ pages/edit/[movieId].tsx
@@ -1,44 +1,24 @@
-import { AxiosResponse } from 'axios'
+import axios, { AxiosResponse } from 'axios'
 import moment from 'moment'
+import { NextPage, NextPageContext } from 'next'
 import Head from 'next/head'
 import { useRouter } from 'next/router'
-import React, { Fragment, useEffect, useState } from 'react'
-import { ResponseType } from '../..'
+import React, { Fragment, useState } from 'react'
 import Form from '../../components/Form/Form'
 import Alert from '../../components/Layout/Alert'
 import useRequest from '../../hooks/use-request'
 import Movie from '../../models/Movie'
 
-const EditMoviePage = () => {
-  const [movieToEdit, setMovieToEdit] = useState<Movie | null>(null)
+interface Props {
+  movie: Movie
+  error: Error
+}
+
+const EditMoviePage: NextPage<Props> = props => {
+  const [movieToEdit, setMovieToEdit] = useState<Movie | null>(props.movie)
   const router = useRouter()
 
   const { isLoading, sendRequest, error, setError } = useRequest()
-
-  useEffect(() => {
-    if (!router.isReady) return
-
-    fetchMovieToEdit()
-  }, [router.isReady])
-
-  const fetchMovieToEdit = async () => {
-    const movieId = router.query.movieId as string
-
-    await sendRequest(
-      {
-        url: `${process.env.NEXT_PUBLIC_API_BASE}/get/${movieId}`,
-        error: 'Failed to fetch movie from the server',
-      },
-      (response: AxiosResponse<any, { url?: string; error: string }>) => {
-        const data: ResponseType<Movie> = response.data
-
-        const date = new Date(data.date)
-        const formattedDate = moment(date).format('YYYY-MM-DD')
-
-        setMovieToEdit({ ...data, date: formattedDate, id: movieId })
-      }
-    )
-  }
 
   const handleEditableMovieSubmit = async (movie: Movie) => {
     await sendRequest(
@@ -52,6 +32,7 @@ const EditMoviePage = () => {
         if (response && response.status === 200) router.push('/')
       }
     )
+    setMovieToEdit(null)
   }
 
   return (
@@ -59,10 +40,10 @@ const EditMoviePage = () => {
       <Head>
         <title>{movieToEdit ? `Edit movie ${movieToEdit.name}` : 'Edit movie'}</title>
       </Head>
-      {error.isError && (
+      {(error.isError || props.error) && (
         <Alert
-          title={error.name}
-          description={error.message}
+          title={props.error.name || error.name}
+          description={props.error.message || error.message}
           onCloseClick={() => {
             setError({ isError: false, name: '', message: '' })
           }}
@@ -78,3 +59,30 @@ const EditMoviePage = () => {
 }
 
 export default EditMoviePage
+
+export async function getServerSideProps(context: NextPageContext) {
+  const movieId = context.query.movieId as string
+
+  let data: Movie | undefined
+  let formattedDate: string | undefined
+  let error: Error | undefined
+  try {
+    const response: AxiosResponse = await axios.get(
+      `${process.env.NEXT_PUBLIC_FIREBASE_URL}/moviesDB/${movieId}.json`
+    )
+    data = response.data
+
+    const date = new Date(data!.date)
+    formattedDate = moment(date).format('YYYY-MM-DD')
+  } catch (err: any) {
+    console.error(`Error in fetching movie from the database: ${err.message && err.message}`)
+    error = {
+      name: 'Error in fetching movie from the database',
+      message: `${err.message && err.message}`,
+    }
+  }
+
+  return {
+    props: { movie: { ...data, id: movieId, date: formattedDate } },
+  }
+}
